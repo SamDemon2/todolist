ceee874ed06864ddb1e2f65657fc404c66bd48a9
SamDemon2 <ugay03@gmail.com>
2024-01-14 20:54:00 +0300

    Создал начальные Header.tsx и MainPage.tsx, перехожу к наполнению контентом

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Header = () => {
+  return <>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
+        <div className="text-white">TO DO LIST</div>
+      </nav>
+    </div>
+  </>;
+};
+export default Header;
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MainPage = () => {
+  return <>
+    <div>
+        SOMETHING TO BE A TEST PART
+    </div>
+  </>;
+};
+export default MainPage;
